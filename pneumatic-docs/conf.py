dfc77e747e89b54142583aff7f3417e85c4dcdea
Kevin Lewis <kevin@surgingsystems.com>
2015-02-28 13:47:53 -0600

    Added some theming

--- pneumatic-docs/conf.py
+++ pneumatic-docs/conf.py
@@ -106,26 +106,43 @@ html_theme = 'default'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+	"stickysidebar": "true",
+
+	"sidebarbgcolor": "#5756a6",
+	"sidebarlinkcolor": "#ffe500",
+	"relbarbgcolor": "#310056",
+	"relbartextcolor": "#ffe500",
+	"relbarlinkcolor": "#ffe500",
+	"footerbgcolor": "#310056",
+	"codebgcolor": "#ede3f1",
+	"headtextcolor": "#310056"
+	
+	#ffe500 # yellow
+	#5756a6 # core purple
+	#c2accb # light purple
+	#ede3f1 # lighter purple
+	#310056 # dark purple
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+html_title = "Pneumatic.IO Reference Guide"
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = "images/SurgingSystemsIcon-Transparent.png"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = None
+html_favicon = "favicon.ico"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
